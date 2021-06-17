e1275cd1106ccfbd15e89bead464be230e4b1c94
sandelinvs@outlook.com <sandelinvs@outlook.com>
2021-06-17 10:15:29 +0200

    htmldownloader stub for now

---
+++ Shared/HTMLDownloader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public interface IHTMLDownloader
+    {
+        Task<string> Download(string url);
+    }
+
+    public class HTMLDownloader : IHTMLDownloader
+    {
+        public async Task<string> Download(string url)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Tests/SetupFixture.cs
+++ Tests/SetupFixture.cs
@@ -8,25 +8,25 @@ namespace Tests
         [SetUp]
         public virtual void BeforeEach()
         {
-            TestContext.WriteLine("BeforeEach");
+            TestContext.WriteLine("BeforeEach"); // Does not print wihout latest updates from nuget
         }
 
         [TearDown]
         public virtual void AfterEach()
         {
-            TestContext.WriteLine("AfterEach");
+            TestContext.WriteLine("AfterEach"); // Does not print wihout latest updates from nuget
         }
 
         [OneTimeSetUp]
         public virtual void BeforeAll()
         {
-            TestContext.WriteLine("BeforeAll"); // does not print to console but does run.
+            TestContext.WriteLine("BeforeAll"); // Is called but does not print to console.
         }
 
         [OneTimeTearDown]
         public virtual void AfterAll()
         {
-            TestContext.WriteLine("AfterAll"); // same as above
+            TestContext.WriteLine("AfterAll"); // Is called but does not print to console.
         }
     }
 }
