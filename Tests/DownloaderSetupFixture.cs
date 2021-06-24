88d6628463c50206c3d3c700af0585432f381917
sandelinvs@outlook.com <sandelinvs@outlook.com>
2021-06-24 16:15:24 +0200

    egen fil per class

--- Tests/DownloaderSetupFixture.cs
+++ Tests/DownloaderSetupFixture.cs
@@ -7,7 +7,6 @@ using System.Reflection;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using Shared;
 using Shared.Services;
@@ -108,30 +107,6 @@ namespace Tests
         public static async void OnComplete(object sender, DownloadEventArgs args)
         {
             TestContext.WriteLine($"Download Completed: {args.Url}");
-        }
-    }
-
-    [TestFixture]
-    public class DownloaderTests : DownloaderSetupFixture
-    {
-        [Test]
-        public async Task Can_Download_booklistpage_to_disk()
-        {
-            var bookStoreUrl = @"https://www.sfbok.se/katalog/bocker-tidningar/romaner-noveller"; ;
-
-            using var downloader = await DownloaderFactory.Create(bookStoreUrl, Path.Combine(LocalFolder, "booklist.html"));
-
-            await downloader.DownloadAsync();
-        }
-
-        [Test]
-        public async Task Can_Download_a_bookcover_to_disk()
-        {
-            var bookStoreImageUrl = @"https://www.sfbok.se/sites/default/files/styles/teaser/sfbok/sfbokbilder/403/403426.jpg?bust=1624032882&itok=WJ8Bb2y1";
-
-            using var downloader = await DownloaderFactory.Create(bookStoreImageUrl, Path.Combine(LocalFolder, "covertart.jpg"));
-
-            await downloader.DownloadAsync();
         }
     }
 }
---
+++ Tests/DownloaderTests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shared.Services;
+
+namespace Tests
+{
+    [TestFixture]
+    public class DownloaderTests : DownloaderSetupFixture
+    {
+        [Test]
+        public async Task Can_Download_booklistpage_to_disk()
+        {
+            var bookStoreUrl = @"https://www.sfbok.se/katalog/bocker-tidningar/romaner-noveller"; ;
+
+            using var downloader = await DownloaderFactory.Create(bookStoreUrl, Path.Combine(LocalFolder, "booklist.html"));
+
+            await downloader.DownloadAsync();
+        }
+
+        [Test]
+        public async Task Can_Download_a_bookcover_to_disk()
+        {
+            var bookStoreImageUrl = @"https://www.sfbok.se/sites/default/files/styles/teaser/sfbok/sfbokbilder/403/403426.jpg?bust=1624032882&itok=WJ8Bb2y1";
+
+            using var downloader = await DownloaderFactory.Create(bookStoreImageUrl, Path.Combine(LocalFolder, "covertart.jpg"));
+
+            await downloader.DownloadAsync();
+        }
+    }
+}
